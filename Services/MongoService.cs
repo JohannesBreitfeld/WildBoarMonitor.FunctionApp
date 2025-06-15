fda8546be1bb117ae28720e59e9273416240d98e
Johannes Breitfeld <jbvbreitfeld@gmail.com>
2025-06-15 22:03:13 +0200

    Update logging

--- Services/MongoService.cs
+++ Services/MongoService.cs
@@ -1,4 +1,5 @@
-﻿using MongoDB.Driver;
+﻿using DnsClient.Internal;
+using MongoDB.Driver;
 using WildboarMonitor.FunctionApp.Models;
 using WildboarMonitor.FunctionApp.Settings;
 
@@ -7,12 +8,14 @@ namespace WildboarMonitor.FunctionApp.Services;
 public class MongoService : IDatabaseService
 {
     private readonly IMongoCollection<ImageAttachment> _collection;
+    private readonly ILogger _logger;
 
-    public MongoService(MongoSettings settings)
+    public MongoService(MongoSettings settings, ILogger logger)
     {
         var client = new MongoClient(settings.ConnectionString);
         var database = client.GetDatabase(settings.DatabaseName);
         _collection = database.GetCollection<ImageAttachment>(settings.CollectionName);
+        _logger = logger;
     }
 
     public async Task InsertResultAsync(ImageAttachment result)
@@ -22,12 +25,20 @@ public class MongoService : IDatabaseService
 
     public async Task<DateTime?> GetLatestTimestampAsync()
     {
-        var sort = Builders<ImageAttachment>.Sort.Descending(x => x.Timestamp);
-        var latest = await _collection.Find(Builders<ImageAttachment>.Filter.Empty)
-                                      .Sort(sort)
-                                      .Limit(1)
-                                      .FirstOrDefaultAsync();
-        return latest?.Timestamp;
+        try
+        {
+            var sort = Builders<ImageAttachment>.Sort.Descending(x => x.Timestamp);
+            var latest = await _collection.Find(Builders<ImageAttachment>.Filter.Empty)
+                                          .Sort(sort)
+                                          .Limit(1)
+                                          .FirstOrDefaultAsync();
+            return latest?.Timestamp;
+        }
+        catch(Exception ex)
+        {
+            _logger.LogError($"Error fetching timestamp from DB. Message: {ex.Message}");
+            return null;
+        }
     }
 }
 
